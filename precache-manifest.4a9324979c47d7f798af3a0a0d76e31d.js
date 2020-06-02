2dac70ab0dce13a394a86b9533ad3b58142fbed7
jwon114 <jthgwong@gmail.com>
2020-06-02 16:42:12 +0100

    Updates

---
+++ precache-manifest.4a9324979c47d7f798af3a0a0d76e31d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7220132d4ee623022afc2835d916224c",
+    "url": "/jwon114/n/index.html"
+  },
+  {
+    "revision": "4214db38a25ce71b74a8",
+    "url": "/jwon114/n/static/css/main.7f35932d.chunk.css"
+  },
+  {
+    "revision": "7b7cc345e05ebddb2669",
+    "url": "/jwon114/n/static/js/2.3c7f9527.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jwon114/n/static/js/2.3c7f9527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4214db38a25ce71b74a8",
+    "url": "/jwon114/n/static/js/main.f4253871.chunk.js"
+  },
+  {
+    "revision": "bb77ee2139df0198402c",
+    "url": "/jwon114/n/static/js/runtime-main.a5494d18.js"
+  },
+  {
+    "revision": "f5c44a2701e74c4fca1f3da7bd120bf9",
+    "url": "/jwon114/n/static/media/loading.f5c44a27.gif"
+  }
+]);
